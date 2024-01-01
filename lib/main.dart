9cb4a6e0e98a6030c3143e5975fe2926e7fc720d
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2024-01-01 16:34:09 +0530

    - Test commit from development branch

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   // Application works only in Portrait Mode by below mentioned code
